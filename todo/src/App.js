be44db17931e21d14d1be833a68ecf4b194db918
shubham9019 <shubham9019@gmail.com>
2020-07-17 20:22:17 +0530

    Update App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(["dos","take rubbish out"]);
+
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <input/>
+      <button>todo</button>
+      <ul>
+        {todos.map(todo=>(
+          <li>{todo}</li>
+        ))}
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   );
 }
